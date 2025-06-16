2cbfa9abceae48a7852c06445ffe85f517c66f3d
anphir <158041111+anphir@users.noreply.github.com>
2025-06-16 21:11:06 +0200

    Support highlighting of tag user

--- languages/comment/highlights.scm
+++ languages/comment/highlights.scm
@@ -8,15 +8,18 @@
 ;   For long multi-line commits this might be a bit excessive, especially for error comments.
 ; TODO: Would be nice if this were an option a user could turned off or on.
 ; TODO: Optional highlighting of the tag user would also be nice.
+;   We could add another style e.g. comment.note.user or simply a comment.user if we want a single style1
+;   for all comment groups.
 (_
     ; Match only on the first occurence of tag.
     . (tag
         (name) @capture @comment.note.name
-        ; Currently the user does not get a specific highlighting.
-        ; "(" @comment.error
-        ; (user) @comment.error
-        ; ")" @comment.error
-        ; ":" @comment.error
+        ; NOTE: The `(user):` part is optional, so match with trailing `?`.
+        ; INFO: Styles for (, ), : chosen according to Rust's `highlighting.scm`.
+        "("? @punctuation.bracket
+        (user)? @comment.note.user
+        ")"? @punctuation.bracket
+        ":"? @punctuation.delimiter
     )
     (#any-of? @capture "NOTE" "INFO")
 ) @comment.note
@@ -25,6 +28,10 @@
 (_
     . (tag
         (name) @capture @comment.todo.name
+        "("? @punctuation.bracket
+        (user)? @comment.todo.user
+        ")"? @punctuation.bracket
+        ":"? @punctuation.delimiter
     )
     (#any-of? @capture "TODO" "WIP")
 ) @comment.todo
@@ -33,6 +40,10 @@
 (_
     . (tag
         (name) @capture @comment.warning.name
+        "("? @punctuation.bracket
+        (user)? @comment.warning.user
+        ")"? @punctuation.bracket
+        ":"? @punctuation.delimiter
     )
     (#any-of? @capture "WARNING" "WARN")
 ) @comment.warning
@@ -41,6 +52,18 @@
 (_
     . (tag
         (name) @capture @comment.error.name
+        "("? @punctuation.bracket
+        (user)? @comment.error.user
+        ")"? @punctuation.bracket
+        ":"? @punctuation.delimiter
     )
     (#any-of? @capture "BUG" "ERROR" "FIXME")
 ) @comment.error
+
+; STASH
+;
+; Highlights all tag names of this comment group.
+; (tag
+;      (name) @capture @comment.note.name
+;      (#any-of? @capture "NOTE" "INFO")
+; )
