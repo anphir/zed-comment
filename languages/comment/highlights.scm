258d8268c96504ee69ac1a116fd97fc24bb36c3a
anphir <158041111+anphir@users.noreply.github.com>
2025-06-18 14:37:11 +0200

    Add tag 'TEST' to todo group

--- languages/comment/highlights.scm
+++ languages/comment/highlights.scm
@@ -1,13 +1,8 @@
-; TODO: tree-sitter-comment supports mostly arbitrary tag names.
-;   Perhaps have a fall-through query highlighting these as NOTE?
-; TODO: Support additional tag names?
-
 ; Note group.
-(_
-    ; Match only on the first occurence of tag.
-    . (tag
-        (name) @capture @comment.note.name
-        ; NOTE: The `(user):` part is optional, so match with trailing `?`.
+(_  ; Match on comment.
+    . (tag  ; Match on the first occurence of a tag.
+        (name) @tag_name @comment.note.name
+        ; NOTE: The `(user)` part is optional, so match with trailing `?`.
         ; INFO: Styles for (, ), : chosen according to Rust's `highlighting.scm`.
         "("? @punctuation.bracket
         (user)? @comment.note.user
@@ -15,44 +10,44 @@
         ":"? @punctuation.delimiter
     )
     (uri)? @link_uri
-    (#any-of? @capture "NOTE" "INFO")
+    (#any-of? @tag_name "INFO" "NOTE")
 ) @comment.note
 
 ; Todo group.
 (_
     . (tag
-        (name) @capture @comment.todo.name
+        (name) @tag_name @comment.todo.name
         "("? @punctuation.bracket
         (user)? @comment.todo.user
         ")"? @punctuation.bracket
         ":"? @punctuation.delimiter
     )
     (uri)? @link_uri
-    (#any-of? @capture "TODO" "WIP")
+    (#any-of? @tag_name "TEST" "TODO" "WIP")
 ) @comment.todo
 
 ; Warning group.
 (_
     . (tag
-        (name) @capture @comment.warning.name
+        (name) @tag_name @comment.warning.name
         "("? @punctuation.bracket
         (user)? @comment.warning.user
         ")"? @punctuation.bracket
         ":"? @punctuation.delimiter
     )
     (uri)? @link_uri
-    (#any-of? @capture "WARNING" "WARN")
+    (#any-of? @tag_name "WARN" "WARNING")
 ) @comment.warning
 
 ; Error group.
 (_
     . (tag
-        (name) @capture @comment.error.name
+        (name) @tag_name @comment.error.name
         "("? @punctuation.bracket
         (user)? @comment.error.user
         ")"? @punctuation.bracket
         ":"? @punctuation.delimiter
     )
     (uri)? @link_uri
-    (#any-of? @capture "BUG" "ERROR" "FIXME")
+    (#any-of? @tag_name "BUG" "ERROR" "FIXME")
 ) @comment.error
